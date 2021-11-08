c426a35ff86ade9665b764065982f12a33fb1f72
Willie <m4ds31@gmail.com>
2021-11-08 20:53:31 +0330

    basic header footer

--- Pysi/builder.py
+++ Pysi/builder.py
@@ -1,11 +1,50 @@
 import os
-import markdown2
 
-testPost = open("./Pysi/Posts/p1.md")
-content = testPost.read()
-output = markdown2.markdown(content)
+tagFile = open("./headers/headers.html")
+tags = tagFile.read()
+tagFile.close()
 
-indexfile = open("./index.html", "w")
-indexfile.write(output)
+homeFile = open("./home/home.html")
+homeContent = homeFile.read()
+homeFile.close()
+
+indexfile = open("../index.html", "w")
+
+# start html
+indexfile.write("\
+    <!doctype html>\
+    <html>\
+")
+
+# write tags
+indexfile.write(tags)
+
+# start body
+indexfile.write("\
+    <body>\
+")
+
+# read header
+headerFile = open("./style/header.html")
+headerContent = headerFile.read()
+headerFile.close()
+indexfile.write(headerContent)
+
+indexfile.write(homeContent)
+
+# read footer
+footerFile = open("./style/footer.html")
+footerContent = footerFile.read()
+footerFile.close()
+indexfile.write(footerContent)
+
+# end of body
+indexfile.write("\
+    </body>\
+")
+
+# end html
+indexfile.write("\
+    </html>\
+")
 indexfile.close()
-testPost.close()
