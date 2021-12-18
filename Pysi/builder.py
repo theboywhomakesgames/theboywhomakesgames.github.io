341becd94a72c10fb88840561df291d204240c11
Willie <m4ds31@gmail.com>
2021-12-18 17:46:27 +0330

    post titles

--- Pysi/builder.py
+++ Pysi/builder.py
@@ -1,5 +1,6 @@
 import os
 from wrapper import wrap
+from jinja2 import Template
 
 homeFile = open("./home/home.html")
 homeContent = homeFile.read()
@@ -12,16 +13,18 @@ post_names = []
 for post_file in os.listdir("./blog"):
     if post_file.endswith(".html"):
         post = open(os.path.join("./blog", post_file))
-        post_content = post.read()
+        post_content = Template(post.read())
+        post_module = post_content.module
         post.close()
-        wrap("../blog/{name}".format(name=post_file), post_content)
-        post_names.append(post_file)
+        wrap("../blog/{name}".format(name=post_file), post_content.render())
+        post_names.append((post_module.title, post_file))
 
 # create the post list
 blog_list = "<ul>"
-for post in post_names:
-    blog_list = blog_list + '<li><a href="/blog/{p}">{p}</a></li>'.format(p=post)
+for post, file_name in post_names:
+    blog_list = blog_list + '<li><h2><a href="/blog/{file}">{p}</a></h2></li>'.format(p=post, file=file_name)
 blog_list = blog_list + "</ul>"
 wrap("../blog.html", blog_list)
 
 # TODO: relative path in header links when using wrapper
+# Right now it works cause I copy pasted the essential css files in all directories!
